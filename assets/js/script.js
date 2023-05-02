fc583dd535575d0a6b253c02317323800b3d477e
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2023-05-02 20:37:34 +0000

    Add code for refresh page and start button for quiz game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 const startButton = document.getElementById('start-button');
-startButton.addEventListener('click', startQuiz)
+const contentMain = document.getElementById('main-content');
+const contentScore = document.getElementById('scoreboard');
+const refreshButton = document.getElementById('refresh');
+refreshButton.addEventListener('click', refreshPage);
+startButton.addEventListener('click', startQuiz);
 
+function refreshPage() {
+    window.location.reload();
+}
 
 
 function startQuiz() {
-
-
+    startButton.classList.add('hide');
+    contentMain.classList.remove('hide');
+    contentScore.classList.remove('hide');
+    console.log(startButton);
 }
 
 function nextQuizQuestions() {
