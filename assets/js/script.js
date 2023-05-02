c2e4a8dbfc12d565bdf7933795b0381b75a90e27
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2023-05-02 16:30:10 +0000

    Add eventListener for next and refresh data type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function(){
+            if (this.getAttribute("data-type") === "next"){
+                alert('nextgame')
+            } else if (this.getAttribute("data-type") === "refresh") {
+                alert('refresh')
+            }
+        })
+    }
+})
+
+
 
