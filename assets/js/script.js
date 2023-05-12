b03eebd99aebb902027b537363c2c4b82874f8de
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2023-05-12 17:43:41 +0000

    Add code show question and array of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,13 @@ const startButton = document.getElementById('start-button');
 const contentMain = document.getElementById('main-content');
 const contentScore = document.getElementById('scoreboard');
 const refreshButton = document.getElementById('refresh');
+const questionElement = document.getElementById('country');
+const answerButtons = document.getElementById('btn');
 refreshButton.addEventListener('click', refreshPage);
 startButton.addEventListener('click', startQuiz);
+
+let shuffledQuestions, currentQuestionIndex
+
 
 function refreshPage() {
     window.location.reload();
@@ -14,13 +19,78 @@ function startQuiz() {
     startButton.classList.add('hide');
     contentMain.classList.remove('hide');
     contentScore.classList.remove('hide');
-    console.log(startButton);
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+    currentQuestionIndex = 0;
+    nextQuizQuestions()
 }
 
 function nextQuizQuestions() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question){
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+
+        button.addEventListener('click', selectAnswer)
+        
+    })
 }
 
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'Ireland',
+        answers: [
+            { text: 'Dublin', correct: true },
+            { text: 'Porto', correct: false },
+            { text: 'Yalta', correct: false },
+            { text: 'Krakow', correct: false }
+        ]
+    },
+    {
+        question: 'Germany',
+        answers: [
+            { text: 'Berlin', correct: true },
+            { text: 'Koln', correct: false },
+            { text: 'London', correct: false },
+            { text: 'Lviv', correct: false }
+        ]
+    },
+    {
+        question: 'France',
+        answers: [
+            { text: 'Paris', correct: true },
+            { text: 'Oslo', correct: false },
+            { text: 'Rome', correct: false },
+            { text: 'Venice', correct: false }
+        ]
+    },
+    {
+        question: 'Poland',
+        answers: [
+            { text: 'Warsaw', correct: true },
+            { text: 'Cork', correct: false },
+            { text: 'Washington', correct: false },
+            { text: 'New York', correct: false }
+        ]
+    },
+    {
+        question: 'Spain',
+        answers: [
+            { text: 'Barcelona', correct: true },
+            { text: 'Pekin', correct: false },
+            { text: 'Tibilicy', correct: false },
+            { text: 'Kyiv', correct: false }
+        ]
+    }
+]
