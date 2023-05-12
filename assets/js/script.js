228943a5df6bb3c04300f27053f1fefe2758bdc0
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2023-05-12 17:58:11 +0000

    Re wrie show question answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,17 +30,6 @@ function nextQuizQuestions() {
 
 function showQuestion(question){
     questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-
-        button.addEventListener('click', selectAnswer)
-        
-    })
 }
 
 function selectAnswer() {
