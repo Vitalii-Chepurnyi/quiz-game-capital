3c4c61eb8b640d5d3b37d555879e3df8b4f8723a
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2023-05-16 13:29:16 +0000

    Add code to function start quiz and add event listener to startButton

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,17 @@ const answerButtons = document.getElementsByClassName('answer-button');
 const nextButton = document.getElementById('submit');
 
 refreshButton.addEventListener('click', refreshPage);
+startButton.addEventListener('click', startQuiz);
 
 function refreshPage() {
     window.location.reload();
 }
 
 function startQuiz() {
-
+    startButton.classList.add('hide');
+    contentMain.classList.remove('hide');
+    contentScore.classList.remove('hide');
+    console.log('startquiz')
 }
 
 function nextQuizQuestions() {
