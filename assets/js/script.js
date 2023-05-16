d68564bb8680e47ab9a8281ff14751cc31db2732
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2023-05-16 13:20:33 +0000

    Add const to tarket all the buttons and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+const startButton = document.getElementById('start-button');
+const contentMain = document.getElementById('main-content');
+const contentScore = document.getElementById('scoreboard');
+const refreshButton = document.getElementById('refresh');
+const questionElement = document.getElementById('country');
+const answerButtons = document.getElementsByClassName('answer-button');
+const nextButton = documnet.getElementById('submit');
+
 function refreshPage() {
 
 }
@@ -19,5 +27,5 @@ function nextQuiz() {
 }
 
 function checkAnswer() {
-    
+
 }
