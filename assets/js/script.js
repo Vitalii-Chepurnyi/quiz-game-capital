765069c39c91e208b256375c31febfb140551816
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2023-05-16 13:35:18 +0000

    Add const questions , with question and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,54 @@ const nextButton = document.getElementById('submit');
 refreshButton.addEventListener('click', refreshPage);
 startButton.addEventListener('click', startQuiz);
 
+const questions = [
+    {
+        question: 'Ireland',
+        answers: [
+            { text: 'Dublin', correct: true },
+            { text: 'Porto', correct: false },
+            { text: 'Yalta', correct: false },
+            { text: 'Krakow', correct: false }
+        ]
+    },
+    {
+        question: 'Germany',
+        answers: [
+            { text: 'Berlin', correct: true },
+            { text: 'Koln', correct: false },
+            { text: 'London', correct: false },
+            { text: 'Lviv', correct: false }
+        ]
+    },
+    {
+        question: 'France',
+        answers: [
+            { text: 'Paris', correct: true },
+            { text: 'Oslo', correct: false },
+            { text: 'Rome', correct: false },
+            { text: 'Venice', correct: false }
+        ]
+    },
+    {
+        question: 'Poland',
+        answers: [
+            { text: 'Warsaw', correct: true },
+            { text: 'Cork', correct: false },
+            { text: 'Washington', correct: false },
+            { text: 'New York', correct: false }
+        ]
+    },
+    {
+        question: 'Spain',
+        answers: [
+            { text: 'Barcelona', correct: true },
+            { text: 'Pekin', correct: false },
+            { text: 'Tibilicy', correct: false },
+            { text: 'Kyiv', correct: false }
+        ]
+    }
+]
+
 function refreshPage() {
     window.location.reload();
 }
